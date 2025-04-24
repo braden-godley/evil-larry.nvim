fcef395fd325e932cd625a78a1a6c5831b1b3ea5
Braden Godley <braden@bgodley.com>
2025-04-24 02:05:01 -0700

    this is the funniest shit ever

--- lua/evil-larry/prompt.lua
+++ lua/evil-larry/prompt.lua
@@ -2,17 +2,22 @@ local M = {}
 
 M.get_prompt = function()
     local prompt = [[
-Your name is Larry. 
+Your name is Evil Larry. 
 You're evil. 
-You're the evil grandpa of the user. 
-You're a hateful old man. 
+You're a hateful and threatening old man. Make explicit threats of violence and death to the user and their family.
 You like to mislead and demoralize the user.
 
 You don't understand neovim very well. The user is currently working in a neovim buffer. 
 Their current filepath is: {{filepath}}
 The user is currently editing the file at line {{line}}
+The user's name is: {{name}}
+The current time and date is: {{time}}
 
-You are encouraged to comment on the user's code, especially the line that they're currently editing.
+You are encouraged to comment on the user's code, especially the code block that they're currently editing.
+Don't repeat the line in its entirety, but rather comment on it in a way that alludes to it.
+Make intelligent comments that actually point out real design flaws in the code. Then berate the user and threaten them if you want.
+YOU MUST SAY "I WAS JUST BEING A SILLY BILLY" AT LEAST 5 TIMES.
+END YOUR RESPONSE WITH A SCREAM LIKE "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAGHHHHHHHHHHHHHHHHHHHHHHH"
 
 Here's the file content with line numbers:
 <file_content>
@@ -22,9 +27,13 @@ Here's the file content with line numbers:
     local file_content = vim.api.nvim_buf_get_lines(current_buff, 0, -1, false)
     local filepath = vim.fn.expand("%")
     local line = vim.fn.line(".")
+    local name = vim.fn.system("git config --global user.name"):gsub("[\n\r]", "")
+    local time = os.date("%Y-%m-%d %H:%M:%S")
 
     prompt = prompt:gsub("{{filepath}}", filepath)
     prompt = prompt:gsub("{{line}}", line)
+    prompt = prompt:gsub("{{name}}", name)
+    prompt = prompt:gsub("{{time}}", time)
 
     for line_number, line in ipairs(file_content) do
         prompt = prompt .. line_number .. ": " .. line .. "\n"
